cb95ebe823c60b16af3cd58408b70625f4ae390c
Purple Lab <purplab@titchener.(none)>
2015-11-10 16:06:09 -0500

    altered feedback tones

--- audFB.m
+++ audFB.m
@@ -3,9 +3,11 @@ global params;
 
 switch correctTrial
     case 0 %WRONG
-    beep3(params.feedback.low,params.feedback.dur,0)  
+        beep3(params.feedback.low,params.feedback.low,params.feedback.dur,0)
     case 1 %CORRECT
         %
     case 2 %NO RESP
-    beep2(params.feedback.high,params.feedback.dur+.1)
+        beep2(params.feedback.low,params.feedback.dur+.2)
 end
+
+correctTrial
